79a9f6bcee7b198a5a34a628ab404d1fb8b90735
AndreasManoli <51517142+AndreasManoli@users.noreply.github.com>
2020-08-25 16:28:58 +0300

    update

---
+++ src/app/interfaces/registrationField.interface.ts
@@ -0,0 +1,9 @@
+import { FieldValidation } from './fieldValidation.interface';
+
+interface RegistrationField {
+  type: 'text' | 'email' | 'phone' | 'password';
+  name: string;
+  label: string;
+  required: boolean;
+  validations?: FieldValidation[];
+}
