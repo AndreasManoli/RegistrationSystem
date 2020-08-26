753c6f2557d5ff49e89bb252d1f2cf8496821ca6
AndreasManoli <51517142+AndreasManoli@users.noreply.github.com>
2020-08-26 22:13:51 +0300

    welcome

--- src/app/modules/welcome/components/welcome/welcome.component.spec.ts
+++ src/app/modules/welcome/components/welcome/welcome.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { WelcomeComponent } from './welcome.component';
 
-fdescribe('WelcomeComponent', () => {
+describe('WelcomeComponent', () => {
   let component: WelcomeComponent;
   let fixture: ComponentFixture<WelcomeComponent>;
 
